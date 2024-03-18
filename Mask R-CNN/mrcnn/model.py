f4d7d0860ceb7484ad6a65d04476b0fc8d19da42
Munazza Usmani <68383952+Munazaa@users.noreply.github.com>
2024-03-18 15:07:02 +0100

    Update model.py

--- Mask R-CNN/mrcnn/model.py
+++ Mask R-CNN/mrcnn/model.py
@@ -87,9 +87,6 @@ def compute_backbone_shapes(config, image_shape):
 #  Resnet Graph
 ############################################################
 
-# Code adopted from:
-# https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
-
 def identity_block(input_tensor, kernel_size, filters, stage, block,
                    use_bias=True, train_bn=True):
     """The identity_block is the block that has no conv layer at shortcut
